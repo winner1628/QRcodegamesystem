faf46459d122d069794ba0b7c456bd80d7586699
winner1628 <wk-leung@vtc.edu.hk>
2026-01-09 11:23:36 +0800

    Add files via upload

--- utils/supabaseClient.js
+++ utils/supabaseClient.js
@@ -3,11 +3,12 @@
  */
 
 const { createClient } = require('@supabase/supabase-js');
+const config = require('../config');
 
 // Create a single supabase client for interacting with your database
 const supabase = createClient(
-  process.env.SUPABASE_URL,
-  process.env.SUPABASE_ANON_KEY
+  config.supabase.url,
+  config.supabase.anonKey
 );
 
 module.exports = supabase;
