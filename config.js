aa99680f8fe382481debeabd2393e0edde002074
winner1628 <wk-leung@vtc.edu.hk>
2026-01-09 22:40:59 +0800

    Update config.js

--- config.js
+++ config.js
@@ -7,7 +7,7 @@ const config = {
   // Supabase 數據庫配置
   supabase: {
     url: 'https://vphihqysgdhdnuszybib.supabase.co',  // 替換為您的Supabase項目URL
-    anonKey: 'sb_publishable_yQ8Br7S-Zk2YdmhtD2dAyg_8Gho4wDS'  // 替換為您的Supabase匿名密鑰
+    anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZwaGlocXlzZ2RoZG51c3p5YmliIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Njc5MjExMjEsImV4cCI6MjA4MzQ5NzEyMX0.85JkUoJw4S7gVIPGn23G4CDaCKhgtzqNQOYDiQEKNps'  // 替換為您的Supabase匿名密鑰
   },
   
   // 系統配置
